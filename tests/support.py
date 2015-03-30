26cf7320cc7a4ca30d3ac4695409730cbad7aae3
Matthew Gamble <matthew.gamble@gmail.com>
2015-03-30 16:45:29 +0100

    Add test support for LZMA, BZIP2 and Deflated compression tests

--- tests/support.py
+++ tests/support.py
@@ -3,12 +3,42 @@ import io
 from tempfile import NamedTemporaryFile, TemporaryFile
 import unittest
 
-# Python<=3.2 doesn't have FileNotFound and NotADirectory 
+# Python<=3.2 doesn't have FileNotFound and NotADirectory
 try:
     FileNotFoundError
 except NameError:
     FileNotFoundError = OSError
     NotADirectoryError = OSError
+
+try:
+    import zlib
+except ImportError:
+    zlib = None
+
+try:
+    import gzip
+except ImportError:
+    gzip = None
+
+try:
+    import bz2
+except ImportError:
+    bz2 = None
+
+try:
+    import lzma
+except ImportError:
+    lzma = None
+
+
+requires_zlib = unittest.skipUnless(zlib, 'requires zlib')
+
+requires_gzip = unittest.skipUnless(gzip, 'requires gzip')
+
+requires_bz2 = unittest.skipUnless(bz2, 'requires bz2')
+
+requires_lzma = unittest.skipUnless(lzma, 'requires lzma')
+
 
 
 TESTFN = '@test'
