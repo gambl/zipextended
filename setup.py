143ad38c1090985962334bb21f8aca3b3831c664
Matthew Gamble <matthew.gamble@gmail.com>
2015-04-07 11:17:06 +0100

    Update setup.py for new package structure and naming

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from distutils.core import setup
 
-setup(name='zipfileext',
+setup(name='zipextended',
       version='0.1',
-      description='ZipFile extension to support remove, rename',
+      description='ZipFile extension to support remove, rename operations',
       author='Matthew Gamble',
       author_email='matthew.gamble@gmail.com',
-      url='https://github.com/gambl/zipfileext',
-      packages=['zipfileext'],
-      package_dir={'zipfileext': '.'},
+      url='https://github.com/gambl/zipextended',
+      packages=['zipextended'],
      )
